469cd3829e46363c5be4ff72bfa709926ca2b61e
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2022-11-08 21:27:27 +0300

    created entity layer and Brand entity

---
+++ src/main/java/com/yavuzahmet/rentacarproject/model/Brand.java
@@ -0,0 +1,13 @@
+package com.yavuzahmet.rentacarproject.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Brand {
+    private int id;
+    private String name;
+}
