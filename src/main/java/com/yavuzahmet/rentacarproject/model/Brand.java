1031ce82ca476176ae4858b959f955902db656d3
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2022-11-08 22:23:08 +0300

    update Brand.java

--- src/main/java/com/yavuzahmet/rentacarproject/model/Brand.java
+++ src/main/java/com/yavuzahmet/rentacarproject/model/Brand.java
@@ -4,10 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+
+    @Column(name = "name")
     private String name;
+
+
 }
