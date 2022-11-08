e4b5e1d5dd44bb39bff1acfa84aecb4b42856aa7
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2022-11-08 21:26:20 +0300

    Initial commit

---
+++ src/main/java/com/yavuzahmet/rentacarproject/RentACarProjectApplication.java
@@ -0,0 +1,13 @@
+package com.yavuzahmet.rentacarproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentACarProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentACarProjectApplication.class, args);
+    }
+
+}
