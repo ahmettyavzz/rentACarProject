069c53ffac5a3c4d679c9d7b238ff7e881d926c5
ahmettyavzz <99591875+ahmettyavzz@users.noreply.github.com>
2022-11-08 22:24:46 +0300

    updated service layer

--- src/main/java/com/yavuzahmet/rentacarproject/service/BrandService.java
+++ src/main/java/com/yavuzahmet/rentacarproject/service/BrandService.java
@@ -1,10 +1,13 @@
 package com.yavuzahmet.rentacarproject.service;
 
 import com.yavuzahmet.rentacarproject.model.Brand;
+import com.yavuzahmet.rentacarproject.service.requests.CreateBrandRequest;
+import com.yavuzahmet.rentacarproject.service.response.GetAllBrandsResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BrandService{
-    List<Brand> getAllBrands();
+    List<GetAllBrandsResponse> getAllBrands();
+    void addBrand(CreateBrandRequest createBrandRequest);
 }
--- src/main/java/com/yavuzahmet/rentacarproject/service/Impl/BrandServiceImpl.java
+++ src/main/java/com/yavuzahmet/rentacarproject/service/Impl/BrandServiceImpl.java
@@ -3,8 +3,11 @@ package com.yavuzahmet.rentacarproject.service.Impl;
 import com.yavuzahmet.rentacarproject.model.Brand;
 import com.yavuzahmet.rentacarproject.repository.BrandRepository;
 import com.yavuzahmet.rentacarproject.service.BrandService;
+import com.yavuzahmet.rentacarproject.service.requests.CreateBrandRequest;
+import com.yavuzahmet.rentacarproject.service.response.GetAllBrandsResponse;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,7 +19,24 @@ public class BrandServiceImpl implements BrandService {
     }
 
     @Override
-    public List<Brand> getAllBrands() {
-        return brandRepository.findAll();
+    public List<GetAllBrandsResponse> getAllBrands() {
+        //brand'i getAllBrandResponse'a mapleme
+
+        List<Brand> brands = brandRepository.findAll();
+        List<GetAllBrandsResponse> brandsResponses = new ArrayList<>();
+        for (Brand brand : brands) {
+            GetAllBrandsResponse responseItem = new GetAllBrandsResponse();
+            responseItem.setId(brand.getId());
+            responseItem.setName(brand.getName());
+            brandsResponses.add(responseItem);
+        }
+        return brandsResponses;
+    }
+
+    @Override
+    public void addBrand(CreateBrandRequest createBrandRequest) {
+        Brand brand = new Brand();
+        brand.setName(createBrandRequest.getName());
+        brandRepository.save(brand);
     }
 }
